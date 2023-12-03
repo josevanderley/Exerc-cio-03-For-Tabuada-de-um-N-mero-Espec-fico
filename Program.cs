4b3774950628eca3081355b87ca35b9609553a6f
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:46:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_03_For_Tabuada_de_um_Número_Específico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do número {numero} de 1 a 10:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+
+                Console.ReadKey();  
+            }
+        }
+    }
+}
+    
